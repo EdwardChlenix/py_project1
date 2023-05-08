bab3f0fe83ca3ec21ba9563e86550bf1f7b15061
Edward <nipetrovskiy@gmail.com>
2023-05-08 19:53:07 +0300

    addition

---
+++ funcs.py
@@ -0,0 +1,5 @@
+def addition():
+    first_number = int(input("Введите первое число\n"))
+    second_number = int(input("Введите второе число\n"))
+    answer = first_number + second_number
+    print(answer)
